4c3c07f9e87143bb4b7358efd7a456bc416a387f
Jordon Bedwell <jordon@envygeeks.io>
2017-12-05 15:12:29 -0600

    Disable integration if Jekyll-Assets is too old.

--- lib/jekyll/reload/hooks/reaction.rb
+++ lib/jekyll/reload/hooks/reaction.rb
@@ -3,18 +3,25 @@
 # Encoding: utf-8
 
 require "jekyll"
-
-begin
-  require "jekyll/assets"
-  Jekyll::Assets::Hook.register :env, :after_write do
-    if Jekyll.env == "development" && jekyll.reloader
-      then jekyll.reloader.reload
-    end
-  end
-rescue LoadError
+def hook
   Jekyll::Hooks.register :site, :post_write, priority: :high do |s|
     if Jekyll.env == "development" && s.reloader
       s.reloader.reload
     end
   end
+end
+
+begin
+  require "jekyll/assets"; require "jekyll/assets/version"
+  if Gem::Version.new(Jekyll::Assets::VERSION) >= Gem::Version.new("3.0")
+    Jekyll::Assets::Hook.register :env, :after_write do
+      if Jekyll.env == "development" && jekyll.reloader
+        then jekyll.reloader.reload
+      end
+    end
+  else
+    hook
+  end
+rescue LoadError
+  hook
 end
--- lib/jekyll/reload/plugins/jekyll/assets/hook.rb
+++ lib/jekyll/reload/plugins/jekyll/assets/hook.rb
@@ -3,9 +3,13 @@
 # Encoding: utf-8
 
 begin
-  require "jekyll/assets"; require_relative "default"
-  Jekyll::Assets::Hook.register :env, :after_init do
-    append_path Pathutil.new(__dir__).join("../../../vendor").expand_path.to_s
+  require "jekyll/assets"; require "jekyll/assets/version"
+  if Gem::Version.new(Jekyll::Assets::VERSION) >= Gem::Version.new("3.0")
+    require_relative "default"
+
+    Jekyll::Assets::Hook.register :env, :after_init do
+      append_path Pathutil.new(__dir__).join("../../../vendor").expand_path.to_s
+    end
   end
 rescue LoadError
   nil
